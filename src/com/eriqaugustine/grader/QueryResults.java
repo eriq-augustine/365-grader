2d18a05eae7b8c14a769c8bd94916df5fd668c3b
Eriq Augustine <eaugusti@chromium.org>
2013-03-30 00:33:44 -0700

    Got some basic query results.

---
+++ src/com/eriqaugustine/grader/QueryResults.java
@@ -0,0 +1,60 @@
+package com.eriqaugustine.grader;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A represetation for the results of a query.
+ */
+public class QueryResults {
+   /**
+    * Ordered data on all of the columns.
+    */
+   private List<ColumnData> columns;
+
+   /**
+    * Each Map in this List represents a single row.
+    * The map is the column name to the data.
+    */
+   private List<Map<String, Object>> rows;
+
+   public QueryResults(ResultSet results) throws java.sql.SQLException {
+      columns = new ArrayList<ColumnData>();
+      rows = new ArrayList<Map<String, Object>>();
+
+      ResultSetMetaData metaData = results.getMetaData();
+      for (int i = 0; i < metaData.getColumnCount(); i++) {
+         columns.add(new ColumnData(metaData.getColumnClassName(i), metaData.getColumnLabel(i)));
+      }
+
+      while (results.next()) {
+         Map<String, Object> row = new HashMap<String, Object>();
+
+         for (ColumnData colData : columns) {
+            row.put(colData.name, results.getObject(colData.name));
+         }
+
+         rows.add(row);
+      }
+   }
+
+   public static class ColumnData {
+      public String name;
+
+      /**
+       * The fully-qualified name of the Java type that represents this column.
+       * Ex: "java.lang.String".
+       */
+      public String type;
+
+      public ColumnData(String name, String type) {
+         this.name = name;
+         this.type = type;
+      }
+   }
+}
